1ca4a3d74345121263d8ea2aaf07d5fc3ee45721
dimassekti <dimassekti.adji@gmail.com>
2022-03-13 20:04:36 +0700

    ChallengeChapter2

---
+++ app/src/main/java/com/coufie/challengechaptertwo/CaseTwo.kt
@@ -0,0 +1,11 @@
+package com.coufie.challengechaptertwo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CaseTwo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_case_two)
+    }
+}
